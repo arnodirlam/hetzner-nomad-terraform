e2cee2d92702518db905c22d9aacf784e160b1cb
Arno Dirlam <arnodirlam@googlemail.com>
2023-01-02 19:14:28 +0100

    Add consul and nomad configs as resources, Restart each on config change

--- servers.tf
+++ servers.tf
@@ -15,6 +15,80 @@ resource "hcloud_server" "server" {
   server_type = var.server_type
   user_data   = local.server_user_data
   labels      = merge(var.labels, { "Name" = var.prefix })
+
+  lifecycle {
+    ignore_changes = [
+      user_data,
+    ]
+  }
+}
+
+resource "remote_file" "consul_config" {
+  conn {
+    host        = local.server_ipv4_addresses[0]
+    user        = "root"
+    private_key = file(var.ssh_key)
+  }
+
+  path        = "/etc/consul.d/consul.hcl"
+  permissions = "0644"
+  content = templatefile("${path.module}/templates/consul.hcl.tmpl", {
+    "servers"   = []
+    "interface" = "eth0"
+  })
+}
+
+resource "null_resource" "consul_config" {
+  connection {
+    host        = local.server_ipv4_addresses[0]
+    user        = "root"
+    private_key = file(var.ssh_key)
+  }
+
+  triggers = {
+    content_hash = sha256(remote_file.consul_config.content)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "systemctl restart consul",
+    ]
+  }
+}
+
+resource "remote_file" "nomad_config" {
+  conn {
+    host        = local.server_ipv4_addresses[0]
+    user        = "root"
+    private_key = file(var.ssh_key)
+  }
+
+  path        = "/etc/nomad.d/nomad.hcl"
+  permissions = "0644"
+  content = templatefile("${path.module}/templates/nomad.hcl.tmpl", {
+    "node_class" = "nomad-server"
+    "datacenter" = "dc1"
+    "servers"    = []
+    "interface"  = "eth0"
+  })
+}
+
+resource "null_resource" "nomad_config" {
+  connection {
+    host        = local.server_ipv4_addresses[0]
+    user        = "root"
+    private_key = file(var.ssh_key)
+  }
+
+  triggers = {
+    content_hash = sha256(remote_file.nomad_config.content)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "systemctl restart nomad",
+    ]
+  }
 }
 
 locals {
